a074966df3c67e5d130fa200cb582f0b6deeb971
Edin Hasagic <edinhassagic@gmail.com>
2024-02-28 14:08:58 +0100

    update

--- restaurant/src/Home/Home.jsx
+++ restaurant/src/Home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Draggable from 'react-draggable';
+import styles from "./Home.module.css";
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div  className={styles.mainbox}>
+        <Draggable bounds="parent">
+          <div className={styles.drag_div}>
+            Drag me within the parent div
+          </div>
+        </Draggable>
+      </div>
+    </>
   )
 }
 
