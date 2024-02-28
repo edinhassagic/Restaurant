1d22b53ef0eb85e8c77b1cefc0386ee5e29b8d4a
Edin Hasagic <edinhassagic@gmail.com>
2024-02-28 15:38:53 +0100

    update

--- restaurant/src/components/Room.jsx
+++ restaurant/src/components/Room.jsx
@@ -2,25 +2,19 @@ import React from "react";
 import styles from "./Room.module.css";
 import Draggable from "react-draggable";
 
-const Room = ()=> {
-
-
+const Room = () => {
   return (
     <div>
       <div className={styles.mainbox}>
         <Draggable bounds="parent">
           <div className={styles.drag_div}>STOL ZA 4</div>
-          
         </Draggable>
         <Draggable bounds="parent">
           <div className={styles.drag_div}>GRUPA LJUDI od 4</div>
-          
         </Draggable>
       </div>
     </div>
-
-
   );
-}
+};
 
 export default Room;
