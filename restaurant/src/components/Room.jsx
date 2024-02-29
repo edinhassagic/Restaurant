c027f2b2b035f0b5a661b0e2e755cac7025796b1
Edin Hasagic <edinhassagic@gmail.com>
2024-02-29 11:20:20 +0100

    update

--- restaurant/src/components/Room.jsx
+++ restaurant/src/components/Room.jsx
@@ -6,12 +6,7 @@ const Room = () => {
   return (
     <div>
       <div className={styles.mainbox}>
-        <Draggable bounds="parent">
-          <div className={styles.drag_div}>STOL ZA 4</div>
-        </Draggable>
-        <Draggable bounds="parent">
-          <div className={styles.drag_div}>GRUPA LJUDI od 4</div>
-        </Draggable>
+       
       </div>
     </div>
   );
