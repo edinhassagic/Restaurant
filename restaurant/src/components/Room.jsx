44a386f48f40477ed6b923e18ed765c0a9a99903
sara <sara12travnik@gmail.com>
2024-02-29 11:46:33 +0100

    dev

--- restaurant/src/components/Room.jsx
+++ restaurant/src/components/Room.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Room.module.css";
-import Draggable from "react-draggable";
+import stylesGroup from "./Group.module.css"
+import { groupData } from "../Data/GroupPeopleData" ;
+import Group from "./Group";
+const Room = ( {handleDragOver, handleDragStart, handleDrop, droppedElement}) => {
+  console.log(droppedElement)
 
-const Room = () => {
   return (
-    <div>
+    <div className={styles.room} onDragOver={handleDragOver} onDrop={handleDrop}>
       <div className={styles.mainbox}>
-        <Draggable bounds="parent">
-          <div className={styles.drag_div}>STOL ZA 4</div>
-        </Draggable>
-        <Draggable bounds="parent">
-          <div className={styles.drag_div}>GRUPA LJUDI od 4</div>
-        </Draggable>
+        {droppedElement.map((element, index) => (
+          <div key={index} className={stylesGroup.groupItem}>
+            <p>Group Name: {element.groupName}</p>
+            <p>Number of People: {element.groupSize}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
