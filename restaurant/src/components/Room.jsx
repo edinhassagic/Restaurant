be4cb0205fb32cf34f6fcb3bf43ffb0bde54330b
Erna Berbic <ernaeniberbic@gmail.com>
2024-02-29 13:28:47 +0100

    dhdfh

--- restaurant/src/components/Room.jsx
+++ restaurant/src/components/Room.jsx
@@ -5,12 +5,36 @@ import { groupData } from "../Data/GroupPeopleData" ;
 import Group from "./Group";
 const Room = ( {handleDragOver, handleDragStart, handleDrop, droppedElement}) => {
   console.log(droppedElement)
+  const [groupPositions, setGroupPositions] = useState({})
+
+  const handleDrag = ( groupName, position) => {
+    setGroupPositions({
+      ...groupPositions,
+      [groupName]: position,
+    });
+  };
 
   return (
     <div className={styles.room} onDragOver={handleDragOver} onDrop={handleDrop}>
       <div className={styles.mainbox}>
         {droppedElement.map((element, index) => (
-          <div key={index} className={stylesGroup.groupItem}>
+          <div 
+            key={index}
+            className={stylesGroup.groupItem}
+            draggable
+            style={{
+              position: "absolute",
+              left: groupPositions[element.groupName]?.x,
+              top: groupPositions[element.groupName]?.y ,
+            }}
+            onDragStart={(e) => {
+              e.dataTransfer.setData("groupName", element.groupName);
+              handleDragStart(e, element.groupName);
+            }}
+            onDrag={(e) => {
+              handleDrag( element.groupName, { x: e.clientX, y: e.clientY });
+            }}
+          >
             <p>Group Name: {element.groupName}</p>
             <p>Number of People: {element.groupSize}</p>
           </div>
