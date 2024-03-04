ee301fd40262b014f29766e94280a50ee904f059
sara <sara12travnik@gmail.com>
2024-03-04 22:02:45 +0100

    ispravljeno dodavanje na osnovu kapaciteta

--- restaurant/src/Home/Home.jsx
+++ restaurant/src/Home/Home.jsx
@@ -9,9 +9,7 @@ import { TableData } from "../Data/TablesData";
 import { v4 as uuidv4 } from "uuid";
 
 const Home = () => {
-
-
-  const parent  = useRef(null)
+  const parent = useRef(null);
   const [droppedElementGroup, setDroppedElementGroup] = useState([]);
   const [droppedElementTable, setDroppedElementTable] = useState([]);
   const [droppedGroupsInTables, setDroppedGroupsInTables] = useState({});
@@ -31,82 +29,94 @@ const Home = () => {
     }
   };
 
-const handleDrop = (e) => {
-  e.preventDefault();
-  const elementId = e.dataTransfer.getData("TableID");
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const elementId = e.dataTransfer.getData("TableID");
 
-  if (e.dataTransfer.getData("tableName")) {
-    const tableName = e.dataTransfer.getData("tableName");
-    const tableIndex = TableData.findIndex(
-      (table) => table.name === tableName
+    if (e.dataTransfer.getData("tableName")) {
+      const tableName = e.dataTransfer.getData("tableName");
+      const tableIndex = TableData.findIndex(
+        (table) => table.name === tableName
+      );
+
+      if (tableIndex != -1) {
+        const tableAlreadyDropped = droppedElementTable.some(
+          (element) => element.name === tableName
+        );
+
+        if (!tableAlreadyDropped) {
+          setDroppedElementTable((prevState) => [
+            ...prevState,
+            { ...TableData[tableIndex], id: uuidv4(), position: {} },
+          ]);
+        }
+      }
+    }
+    if (elementId) {
+      const parentRect = parent.current.getBoundingClientRect();
+      setDroppedElementTable((prevState) =>
+        prevState.map((element) => {
+          if (element.id == elementId) {
+            const newX = e.clientX - parentRect.left;
+            const newY = e.clientY - parentRect.top;
+            return { ...element, position: { x: newX, y: newY } };
+          }
+          return element;
+        })
+      );
+    }
+  };
+
+  const handleTableDropTable = (event, id) => {
+    event.preventDefault();
+    const groupName = event.dataTransfer.getData("groupName");
+    const droppedGroup = groupData.find(
+      (group) => group.groupName === groupName
+    );
+    console.log(droppedGroup)
+    if (!droppedGroup || droppedGroupsInTables[groupName]) return;
+
+    let updatedDroppedElementGroup = [...droppedElementGroup];
+    let updatedDroppedElementTable = [...droppedElementTable];
+    const tableIndex = updatedDroppedElementTable.findIndex(
+      (table) => table.id === id
     );
 
-    if (tableIndex != -1)
-      setDroppedElementTable([
-        ...droppedElementTable,
-        { ...TableData[tableIndex], id: uuidv4(), position: {} },
-      ]);
-  }
- if (elementId) {
+    if (tableIndex === -1) return;
+    console.log(droppedElementTable[tableIndex])
+    const tableCapacity = updatedDroppedElementTable[tableIndex].capacity;
+    const totalGroupSize = updatedDroppedElementGroup
+    .filter((group) => group.targetedTable === id)
+    .reduce((total, group) => total + group.groupSize, 0);
 
-  const parentRect = parent.current.getBoundingClientRect(); 
-  setDroppedElementTable(prevState => prevState.map(element => {
-    if (element.id == elementId) {
-      const newX = e.clientX - parentRect.left 
-      const newY = e.clientY - parentRect.top
-      return { ...element, position: { x: newX, y: newY } };
+    // Proveravamo da li nova grupa može stati u sto uzimajući u obzir preostali kapacitet stola nakon dodavanja prethodnih grupa
+    console.log(tableCapacity ,  totalGroupSize);
+    if (droppedGroup.groupSize <= tableCapacity - totalGroupSize +1) {
+      updatedDroppedElementGroup = [
+        ...updatedDroppedElementGroup,
+        { ...droppedGroup, targetedTable: id },
+      ];
+      setDroppedElementGroup(updatedDroppedElementGroup);
+      setDroppedGroupsInTables((prev) => ({ ...prev, [groupName]: id }));
+    } else {
+      console.log("Kapacitet stola je premali za grupu!");
     }
-    return element;
-  }));
-}
-
-};
-
-const handleTableDropTable = (event, id) => {
-  event.preventDefault();
-  const groupName = event.dataTransfer.getData("groupName");
-  const droppedGroup = groupData.find((group) => group.groupName === groupName);
-
-  if (!droppedGroup || droppedGroupsInTables[groupName]) return;
-
-  let updatedDroppedElementGroup = [...droppedElementGroup];
-  let updatedDroppedElementTable = [...droppedElementTable];
-  const tableIndex = updatedDroppedElementTable.findIndex((table) => table.id === id);
-
-  if (tableIndex === -1) return;
-
-  const tableCapacity = updatedDroppedElementTable[tableIndex].capacity;
-  const totalGroupSize = updatedDroppedElementGroup.reduce((total, group) => total + group.groupSize, 0);
-
-  // Proveravamo da li nova grupa može stati u sto uzimajući u obzir preostali kapacitet stola nakon dodavanja prethodnih grupa
-  console.log(tableCapacity - totalGroupSize)
-  if (droppedGroup.groupSize <= tableCapacity - totalGroupSize) {
-    updatedDroppedElementGroup = [
-      ...updatedDroppedElementGroup,
-      { ...droppedGroup, targetedTable: id },
-    ];
-    setDroppedElementGroup(updatedDroppedElementGroup);
-    setDroppedGroupsInTables((prev) => ({ ...prev, [groupName]: id }));
-  } else {
-    console.log("Kapacitet stola je premali za grupu!");
-  }
-};
+  };
 
   return (
     <div className={styles.home}>
       <Group handleDragStart={handleDragStart} />
       <div ref={parent}>
-      <Room
-        handleDragOver={handleDragOver}
-        handleDragStart={handleDragStart}
-        handleDrop={handleDrop}
-        handleDropTable={handleTableDropTable}
-        droppedElementTable={droppedElementTable}
-        droppedElementGroup={droppedElementGroup}
-        setDroppedElementTable={setDroppedElementTable}
-      />
-
-</div>
+        <Room
+          handleDragOver={handleDragOver}
+          handleDragStart={handleDragStart}
+          handleDrop={handleDrop}
+          handleDropTable={handleTableDropTable}
+          droppedElementTable={droppedElementTable}
+          droppedElementGroup={droppedElementGroup}
+          setDroppedElementTable={setDroppedElementTable}
+        />
+      </div>
       <Table
         handleDragStart={handleDragStart}
         droppedElementGroup={droppedElementGroup}
